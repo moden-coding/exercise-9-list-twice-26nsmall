6db136aa9372043a57dc5670a3eff38ca0aea2cc
26nsmall <26nsmall@gannacademy.org>
2025-09-02 15:55:33 -0400

    comma

--- src/list_twice.py
+++ src/list_twice.py
@@ -1,3 +1,11 @@
 # Write your solution here
 list = []
-
+n = 1
+while n != 0:
+    n = int(input("New item: " ))
+    if n == 0:
+        print("Bye!")
+    else:
+        list.append(n)
+        print(f"The list now: {list}")
+        print(f"The list in order: {sorted(list)}")
